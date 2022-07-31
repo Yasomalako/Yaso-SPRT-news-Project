94fe0e10ed12b220b1468b1f45402c7a114ebc74
Yasomalako <Yasomalako@gmail.com>
2022-07-31 09:39:50 +0300

    kick

--- games/football/FOOTBAL.js
+++ games/football/FOOTBAL.js
@@ -31,40 +31,38 @@ const options = {
 //     })
 // 	.catch(err => console.error(err));
 
-/         let url = `https://my-json-server.typicode.com/Jeck99/fake-server/devices`
-// let users = `https://my-json-server.typicode.com/Jeck99/fake-server/users`
 
-// async function getUsersApi() {
-//     try {
-//       const result= await fetch(users)
-//     return result.json()    
+async function getUsersApi() {
+    try {
+    //   const result= await fetch(`https:api.openligadb.de/getmatchdata/bl1/2020/8`)
+    return result.json()    
     
-//     }  
-//     catch (err) {
-//         alert("sory we got error")
-//     }
-//     finally { }
-// }getUsersApi().then(res => console.log(res))
+    }  
+    catch (err) {
+        alert("sory we got error")
+    }
+    finally { }
+}getUsersApi().then(res => console.log(res))
 
 
-// const tablebody = document.getElementById("users_data");
-// function usersTable() {
-//     getUsersApi()
-//         .then(result => {
-//             result.forEach(item => {
-//                 tablebody.innerHTML +=
-//                     `
-//                 <tr>
-//                 <td>${item.age}</td>
-//                 <td>${item.name.first}</td>
-//                 <td>${item.name.last}</td>
-//                 <td>${item.Email}</td>
-//                 <td>${item.index}</td>
-//                 <td>${item.phone}</td>
-//                 <td>${item.picture}</td>
-//                 </tr>
-//                 `
-//             });
-//         })
-// }
-// usersTable();
+const tablebody = document.getElementById("users_data");
+function usersTable() {
+    getUsersApi()
+        .then(result => {
+            result.forEach(item => {
+                tablebody.innerHTML +=
+                    `
+                <tr>
+                <td>${item.age}</td>
+                <td>${item.name.first}</td>
+                <td>${item.name.last}</td>
+                <td>${item.Email}</td>
+                <td>${item.index}</td>
+                <td>${item.phone}</td>
+                <td>${item.picture}</td>
+                </tr>
+                `
+            });
+        })
+}
+usersTable();
